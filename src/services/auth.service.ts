954c226705ffd20649952dc75c05d064d8f6d2e6
Frédéric TRAN <frederictran@hotmail.fr>
2024-02-25 16:06:58 +0100

    fix: Typing req.user on /api/auth/discord/callback

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,7 +4,7 @@ import { UserModel } from '@/models/user.model';
 
 @Service()
 export class AuthService {
-  public async callback(reqUser): Promise<void> {
+  public async callback(reqUser: { id: string; username: string }): Promise<void> {
     const user: User = {
       discordId: reqUser.id,
       username: reqUser.username,
