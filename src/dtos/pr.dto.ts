10b870b4b659f8606f6ab039517c3b26af888d81
Frédéric TRAN <frederictran@hotmail.fr>
2024-08-16 18:47:02 +0200

    feat: More DTO

--- src/dtos/pr.dto.ts
+++ src/dtos/pr.dto.ts
@@ -1,29 +1,42 @@
-import { IsArray, IsBoolean, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsArray, IsBoolean, IsNotEmpty, IsNumber, IsString, ValidateNested, isString } from 'class-validator';
 
-import { Song } from "@/interfaces/song.interface";
+import { Song } from '@/interfaces/song.interface';
+import { Type } from 'class-transformer';
 
 export class CreatePRDto {
-    @IsString()
-    @IsNotEmpty()
-    public name: string;
+  @IsString()
+  @IsNotEmpty()
+  public name: string;
 
-    @IsString()
-    @IsNotEmpty()
-    public creator: string;
+  @IsBoolean()
+  @IsNotEmpty()
+  public nomination: boolean;
 
-    @IsBoolean()
-    @IsNotEmpty()
-    public nomination: boolean;
+  @IsBoolean()
+  @IsNotEmpty()
+  public blind: boolean;
 
-    @IsBoolean()
-    @IsNotEmpty()
-    public blind: boolean;
+  @IsNumber()
+  @IsNotEmpty()
+  public deadline: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    public deadline: number;
+  @IsArray()
+  @IsNotEmpty()
+  @Type(() => SongListDto)
+  @ValidateNested({ each: true })
+  public songList: Song[];
+}
 
-    @IsArray()
-    @IsNotEmpty()
-    public songList: Song[];
+export class SongListDto {
+  @IsString()
+  @IsNotEmpty()
+  public artist: string;
+  
+  @IsString()
+  @IsNotEmpty()
+  public title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public urlVideo: string;
 }
