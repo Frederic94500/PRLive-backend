328bba03e7bb8278c3bb15936e124390d69c415b
Frédéric TRAN <frederictran@hotmail.fr>
2024-10-01 02:54:07 +0200

    fix: Enforce enum in Mongoose

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,7 +8,7 @@ const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   role: { type: String, required: true },
-  server: { type: String, required: true },
+  server: { type: String, enum: ['EU', 'NA1', 'NA2'], default: 'EU', required: true },
 });
 
 export const UserModel = model<User & Document>('User', UserSchema);
